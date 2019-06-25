fbb508e028037be1c86f00dcc358a6aa75f03d53
timotheein <easer.ceo@gmail.com>
2019-06-25 10:03:24 +0200

    fbsdk added for android fbsdk added for ios firebase added for ios

--- android/app/src/main/java/com/easerpod/MainApplication.java
+++ android/app/src/main/java/com/easerpod/MainApplication.java
@@ -2,6 +2,7 @@ package com.easerpod;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
 import com.facebook.react.ReactApplication;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -15,6 +16,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -25,7 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
+            new FBSDKPackage(mCallbackManager),
             new RNFirebasePackage()
       );
     }
