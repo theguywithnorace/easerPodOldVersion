aa07b60a310379bc81288dc7f03294eb316e1ae7
timotheein <easer.ceo@gmail.com>
2019-06-26 22:09:18 +0200

    +vector icons + ratings

--- android/app/src/main/java/com/easerpod/MainApplication.java
+++ android/app/src/main/java/com/easerpod/MainApplication.java
@@ -7,6 +7,7 @@ import com.airbnb.android.react.maps.MapsPackage;
 
 import com.facebook.CallbackManager;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.github.reactnativecommunity.location.RNLocationPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNLocationPackage(),
             new RNGestureHandlerPackage(),
             new MapsPackage(),
